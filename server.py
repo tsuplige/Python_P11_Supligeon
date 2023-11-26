aac010b3182f0c8eebb40d5b9de037cd4b16b5bc
Tsuplige <thomassupligeon@gmail.com>
2023-11-26 21:45:10 +0100

    deduce point in PurchasePlaces()

--- server.py
+++ server.py
@@ -100,6 +100,7 @@ def purchasePlaces():
         and placesRequired <= 12
         and placesRequired <= int(competition["numberOfPlaces"])
     ):
+        club["points"] = str(int(club["points"]) - placesRequired)
         competition["numberOfPlaces"] = (
             int(competition["numberOfPlaces"]) - placesRequired
         )
